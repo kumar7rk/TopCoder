050d48eb6b857418bf5dd8c866452f9880e0a343
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-03-08 12:21:02 +1030

    WritingWords solved

---
+++ WritingWords.java
@@ -0,0 +1,17 @@
+package TopCoder;
+public class WritingWords{
+	public static void main(String[]args){
+		String word = "CFIRJCDDSOZOYVEGURFWCSFMOXEQMRJOWRGHWLKOBMEAHKGCCN";
+		int res = write(word);
+		System.out.println(res);
+	}
+	public static int write(String word){
+		int sum = 0;
+		for(int i = 0; i < word.length();i++){
+			char c = word.charAt(i);
+			int value = (int) c-'A'+1;
+			sum += value;
+		}
+		return sum;
+	}
+}
