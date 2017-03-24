aad165a345dede3f96ea3dc467bbdb4ec8fd1820
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-03-24 20:28:04 +1030

    CoinFlipsDiv2 added

---
+++ CoinFlipsDiv2.java
@@ -0,0 +1,27 @@
+package TopCoder;
+
+public class CoinFlipsDiv2 {
+
+	public static void main(String[] args) {
+		String state = 
+				"THTTTTTTTTTTTTTHTTHTTTTTHHHTTTTHTTTTTTTTTT"	
+				;
+		int res = countCoins(state);
+		System.out.println(res);
+	}
+	public static int countCoins(String state){
+		int count = 0;
+		if (state.charAt(0)!=state.charAt(1) || state.charAt(state.length()-2)!=state.charAt(state.length()-1)) count++;
+		for (int i = 1; i < state.length()-1; i++) {
+			char c_1 = state.charAt(i-1);
+			char c1 = state.charAt(i);
+			char c2 = state.charAt(i+1);
+		
+			if (c1!=c2 || c1!=c_1) {
+				count++;
+			}
+		}
+		
+		return count;
+	}
+}
