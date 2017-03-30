81821000f620c62ef7808c8421891458b1f57923
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-03-30 12:25:39 +1030

    HexSpeak added

---
+++ HexSpeak.java
@@ -0,0 +1,15 @@
+package TopCoder;
+public class HexSpeak {
+
+	public String decode(long cipherText){
+		
+		String hex = Long.toHexString(cipherText);
+		for (int i = 2; i < 10; i++) {
+			if (hex.contains(i+"")) return "Error!";
+		}
+		hex = hex.replaceAll("1", "I");
+		hex = hex.replaceAll("0", "O");
+		
+		return hex.toUpperCase();
+	}
+}
