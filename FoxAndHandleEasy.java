88351c881163fe7e5355e5c70532efa1652fc86e
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-03-31 17:22:51 +1030

    FoxAndHandlEasy added

---
+++ FoxAndHandleEasy.java
@@ -0,0 +1,27 @@
+package TopCoder;
+
+public class FoxAndHandleEasy {
+
+	public static void main(String[] args) {
+
+		String S = 
+				"Ciel"
+				;
+		String T = 
+				"CCieliel"	
+				;
+		String res = isPossible(S,T);
+		System.out.println(res);
+		
+	}
+	public static String isPossible(String S, String T){
+
+		for (int i = 0; i < S.length(); i++) {
+			String ST = S.substring(0,i)+S+S.substring(i);
+			if (ST.equals(T)) return "Yes";
+		}
+		return "No";
+		
+	}
+
+}
