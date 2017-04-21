557968f0c27d596d7cd74b6b4bb1ad8387d0c9d2
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-04-21 15:15:37 +0930

    SuperUserDo added

---
+++ SuperUserDo.java
@@ -0,0 +1,24 @@
+package TopCoder;
+
+import java.util.HashSet;
+
+public class SuperUserDo {
+
+	public static void main(String[] args) {
+		int[] A = {1,1};
+		int[] B = {1,1};
+		
+		int res = install(A,B);
+		System.out.println(res);
+	}
+	public static int install(int[] A, int[] B) {
+
+		HashSet<Integer> hashSet = new HashSet<>();
+		for (int i = 0; i < B.length; i++)
+			for (int j = A[i]; j <= B[i]; j++)
+				hashSet.add(j);
+		return hashSet.size();
+	}
+	
+
+}
