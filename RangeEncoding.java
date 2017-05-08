93eb14e3d6921f009affd3a5bb848b02191105d8
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-05-08 14:42:18 +0930

    RangeEncoding refactoring

--- RangeEncoding.java
+++ RangeEncoding.java
@@ -5,9 +5,11 @@ public class RangeEncoding {
 	public static void main(String[] args) {
 		int[] arr = {2,4,5,6,8,9,10,11,12,15}
 ;
+		int res = minRanges(arr);
 		System.out.println(res);
 	}
 
+	public static int minRanges(int[] arr) {
 		int count = 1;
 		for (int i = 0; i < arr.length-1; i++) {
 			int current = arr[i];
