53d21aa68232de6765eba9f2c1fe3d57577eef78
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-05-08 14:41:08 +0930

    RangeEncoding added

---
+++ RangeEncoding.java
@@ -0,0 +1,22 @@
+package TopCoder;
+
+public class RangeEncoding {
+
+	public static void main(String[] args) {
+		int[] arr = {2,4,5,6,8,9,10,11,12,15}
+;
+		System.out.println(res);
+	}
+
+		int count = 1;
+		for (int i = 0; i < arr.length-1; i++) {
+			int current = arr[i];
+			int next = arr[i+1];
+			if (next!=current+1) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+}
