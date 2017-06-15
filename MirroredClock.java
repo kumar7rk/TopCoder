257ea98b9ada0bb4ca440b8953ffa62c040cf2f0
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-06-15 16:59:59 +0930

    MirroredClock Refactoring

--- MirroredClock.java
+++ MirroredClock.java
@@ -9,20 +9,17 @@ public class MirroredClock {
 	}
 
 	public static String whatTimeIsIt(String time) {
-		String actualH = "12";
-		String actualM = "59";
-		
 		String [] timeHM = time.split(":");
 		String hours = timeHM[0];
 		String minutes = timeHM[1];
 		
-		if (hours.equals("00") && minutes.equals("00"))  return "00:00";
-		if(Integer.parseInt(minutes)!=0)
-			hours = (Integer.parseInt(hours)+1)+"";
+		if (hours.equals("00") && minutes.equals("00")) return "00:00";
 		
-		String H = Math.abs((Integer.parseInt(actualH)-Integer.parseInt(hours)))+"";
-		String M = Math.abs((Integer.parseInt(actualM)-Integer.parseInt(minutes)))+"";
-		M = (Integer.parseInt(M)+1)+"";				
+		if(Integer.parseInt(minutes)!=0) hours = (Integer.parseInt(hours)+1)+"";
+		
+		String H = Math.abs((12-Integer.parseInt(hours)))+"";
+		String M = Math.abs((60-Integer.parseInt(minutes)))+"";
+		
 		if (M.equals("60")) M = "00";
 		if (H.length()==1) H = "0".concat(H);
 		if (M.length()==1) M = "0".concat(M);
