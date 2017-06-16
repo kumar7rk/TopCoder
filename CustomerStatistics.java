2fb27a5fd4de244d86c4004d246a5eefa4b8de01
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-06-16 19:53:44 +0930

    CustomerStatistics added without arena testing

---
+++ CustomerStatistics.java
@@ -0,0 +1,46 @@
+package TopCoder;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CustomerStatistics {
+
+	public static void main(String[] args) {
+		String[] customerNames = 
+			{"ANDREW", "BILL", "CINDY", "DOH", "ERGH", "FOO", "GOO", "HMPH"}
+		;
+		String [] res = reportDuplicates(customerNames);
+		for (String s: res) System.out.println(s);
+	}
+
+	public static String[] reportDuplicates(String[] customerNames) {
+		int count = 0;
+		HashMap<String, Integer> hashMap = new HashMap<>();
+		for (String s : customerNames) {
+			if (hashMap.containsKey(s)) hashMap.put(s, hashMap.get(s)+1);
+			else
+				hashMap.put(s,1);
+		}
+		int counter = 0;
+		Map<String, Integer> map = new TreeMap<String, Integer>(hashMap);
+		Iterator it = map.entrySet().iterator();
+	    while (it.hasNext()) {
+	        Map.Entry pair = (Map.Entry)it.next();
+	        if((int)pair.getValue()>1){
+	        	counter++;
+	        }
+	    }
+	    String[] res = new String[counter];
+	    Iterator it1 = map.entrySet().iterator();
+	    while (it1.hasNext()) {
+	        Map.Entry pair = (Map.Entry)it1.next();
+	        if((int)pair.getValue()>1){
+	        }
+	    }
+		return res;
+		
+	}
+
+}
