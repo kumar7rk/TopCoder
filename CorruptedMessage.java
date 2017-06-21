7708c94da691239d66240289cbeed08b46f3fe40
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-06-22 08:06:19 +0930

    CorruptedMessage added

--- CorruptedMessage.java
+++ CorruptedMessage.java
@@ -4,56 +4,32 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-import icsV1dot1.Time;
-
 public class CorruptedMessage {
 
 	public static void main(String[] args) {
-		String s = "xksyvkfsxikhdjdfmqlsmvdijobopdqgnvpqugka";
+		//xksyvkfsxikhdjdfmqlsmvdijobopdqgnvpqugka
+		String s = "abc";
 //		System.out.println(s.length());
-		int k = 40;
-		String result = reconstructMessage1(s,k);
+		int k = 3;
+		String result = reconstructMessage(s,k);
 		System.out.println(result);
 	}
 
-	private static String reconstructMessage(String s, int k) {
-
-		HashMap<Character, Integer> hashMap = new HashMap<>();
-		char c1[];
-		c1 = s.toCharArray( );
-		for(int i = 0; i<s.length(); i++){
-			for(int j = 97;j <= 122;j++) 
-				if(c1[i] == j){
-				if (hashMap.containsKey(c1[i])) {
-					int value = hashMap.get(c1[i]);
-					hashMap.put(c1[i], ++value);
-				}
-				else {
-					hashMap.put(c1[i], 1);
-				}
-			}
-		}
-		System.out.println(hashMap);
+	public static String reconstructMessage(String s, int k) {
 		String result = "";
-		Character tempKey = 'a'; 
-		int intKey = 1;
-		Iterator entries = hashMap.entrySet().iterator();
-		while (entries.hasNext()) {
-		    Map.Entry entry = (Map.Entry) entries.next();
-		    Character key = (Character)entry.getKey();
-		    Integer value = (Integer)entry.getValue();
-		    intKey = key+1;
-		    tempKey = (char) intKey;
-//		    System.out.println("Key = " + key + ", Value = " + value);
-		    if (value+k==s.length()) {
-		    	for (int i = 0; i < s.length(); i++) {
-					result+=key+"";
+		int[] used = new int[256];
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			used[c]++;
+		}
+		char c = 'a';
+		for (int i = 'a'; i <='z'; i++) {
+			if (used[i]+k==s.length()) {
+				for (int j = 0; j < s.length(); j++) {
+					result+=(char)i+"";
 				}
 				return result;
 			}
-		}
-		for (int i = 0; i < s.length(); i++) {
-			result+=tempKey+"";
 		}
 		return result;
 	}
