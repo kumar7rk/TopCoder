601097256d4aa9432072a577fcbc2087a8b0ba61
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-06-29 19:21:28 +0930

    Refactoring DistinguishableSetDiv2

--- DistinguishableSetDiv2.java
+++ DistinguishableSetDiv2.java
@@ -1,14 +1,30 @@
 package TopCoder;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DistinguishableSetDiv2 {
 
-	static ArrayList<String> arrayList = new ArrayList<>();
 	public static void main(String[] args) {
+		String[] answer = {"HGHHGUHUHI", "BQHJWOSZMM", "NDKSKCNXND", "QOEOEIWIDS", "IIQIWUNNZM"};
+		int res = count(answer);
+		System.out.println(res);
 	}
-	private static int count(String[] answer) {
+	public static int count(String[] answer) {
+		int N= answer.length;
+		int M= answer[0].length();
 		int counter = 0;
-		
+		for (int i = 0; i < (1<<M); i++) {
+			Set<String> set = new HashSet<>();
+			boolean valid = true;
+			for (int j = 0; j < N&&valid; j++) {
+				StringBuilder builder = new StringBuilder();
+				for (int k = 0; k < M; k++) 
+					if (((i>>k)&1)==1) builder.append(answer[j].charAt(k));
+				valid = set.add(builder.toString());
+			}
+			if (valid) counter++;
+		}
 		return counter;
 	}
+}
