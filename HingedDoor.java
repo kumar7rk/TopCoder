c2b03a7ab5caa77acaaf187b1c025a5586fa6cd1
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-07-10 22:25:05 +0930

    HingedDoor added

---
+++ HingedDoor.java
@@ -0,0 +1,25 @@
+package TopCoder;
+
+public class HingedDoor {
+
+	public static void main(String[] args) {
+		int initialAngle = 76;
+		int reduction = 2;
+		int res = numSwings(initialAngle,reduction);
+		System.out.println(res);
+
+	}
+
+	public static int numSwings(int initialAngle, int reduction) {
+		int res = 0;
+		double val = initialAngle;
+		double r = reduction;
+		while(val>5.0){
+			val = val*(1/r); 
+			System.out.println(val);
+			res++;
+		}
+		return res;
+	}
+
+}
