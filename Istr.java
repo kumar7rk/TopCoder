3d5667e119d66b3ea5a045875b7fa956423656ad
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-08-03 13:43:34 +0930

    Istr optimised for arena

--- Istr.java
+++ Istr.java
@@ -1,7 +1,6 @@
 package TopCoder;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 public class Istr {
 
@@ -12,65 +11,22 @@ public class Istr {
 		System.out.println(result);
 	}
 
-	public static int count(String s, int k) {
-		ArrayList<Integer> arrayList = new ArrayList<>();
-		int[] count = new int[26];
-		int result = 0;
-		int max = 0;
-		frequency(s, count);
-		max = getArrayList(arrayList, count, max);
-		k = newArray(k, arrayList);
-		return getSum(arrayList, result);
-	}
-
-	public static int getArrayList(ArrayList<Integer> arrayList, int[] count, int max) {
-		for (int val : count) {
-			max = max < val ? val : max ;
-			if (val>0){
-				arrayList.add(val);
-			}
-		}
-		return max;
-	}
-
-	public static void frequency(String s, int[] count) {
-		char c;
-		for (char i = 'a'; i < 'z'; i++) {
-			for (int j = 0; j < s.length(); j++) {
-				c = s.charAt(j);
-				if (c==i) 
-					count[i-'a']++;
-			}
-		}
-	}
-
-	public static int newArray(int k, ArrayList<Integer> arrayList) {
+	public static int count(String s, int k){
+		int[]frequency = new int[26];
+		int totalChar = 0;
+		int count = 0;
+		int res = 0;
+		for (char c : s.toCharArray()) frequency[c-'a']++;
+		for (int i = 0; i < frequency.length; i++) if (frequency[i]!=0) totalChar++;
+		int[] occ = new int[totalChar];
+		for (int i = 0; i < frequency.length; i++) if (frequency[i]!=0) occ[count++] = frequency[i];
+		Arrays.sort(occ);
 		while (k>0) {
-			Collections.sort(arrayList);
-			Collections.reverse(arrayList);
-//			System.out.println(arrayList);
-			int a1 = arrayList.get(0);
-			int a2 = arrayList.get(1);
-			int diff = a1-a2+1;
-//			System.out.println("diff:"+diff);
-			if (diff>k){
-				k-=(diff-1);
-				arrayList.set(0, a2);
-			}
-			if (k>0){				
-				k -= diff;
-				arrayList.set(0, a2-1);
-			}
-//			System.out.println("k:"+k);
+			occ[occ.length-1] = --occ[occ.length-1];
+			Arrays.sort(occ);
+			k--;
 		}
-		return k;
-	}
-
-	public static int getSum(ArrayList<Integer> arrayList, int result) {
-		for (Integer integer : arrayList) {
-//			System.out.print(integer+" ");
-			result+=integer*integer;
-		}
-		return result;
+		for (int i : occ) res+=i*i;
+		return res;
 	}
 }
