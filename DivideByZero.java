31044440698115fa3e5f230c19bf567692eac00e
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-08-03 21:16:32 +0930

    DivideByZero optimised for arena

--- DivideByZero.java
+++ DivideByZero.java
@@ -1,31 +1,41 @@
 package TopCoder;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class DivideByZero {
 
 	public static void main(String[] args) {
-		int[] numbers = {95, 17, 7, 86, 31, 11, 74, 45, 79, 36};
+		int[] numbers = 
+			{2, 100}			
+		;
 		int res = CountNumbers(numbers);
 		System.out.println(res);
 	}
 
 	public static int CountNumbers(int[] numbers) {
-		int res = addNumber(numbers);
-		return res;
-	}
-
-	public static int addNumber(int[] numbers) {
 		ArrayList<Integer> arrayList = new ArrayList<>();
 		for (Integer integer : numbers) arrayList.add(integer);
 		for (int i = 0; i < arrayList.size(); i++) {
 			for (int j = 0; j < arrayList.size(); j++) {
+				Collections.sort(arrayList);
+				if (i==j) continue;
 				if (arrayList.get(j)!=0){
 					int div = arrayList.get(i)/arrayList.get(j);
-					if (!arrayList.contains(div)&&div!=0) arrayList.add(div);
+//					System.out.println(div+ " "+i + " "+j);
+					if (!arrayList.contains(div)&&div!=0){
+						arrayList.add(div);
+						i = 0;
+						j = 0;
+					}
 				}
 			}
 		}
+		Collections.sort(arrayList);
+		for (Integer integer : arrayList) {
+			System.out.print(integer+ " ");
+		}
+		System.out.println();
 		return arrayList.size();
 	}
 }
