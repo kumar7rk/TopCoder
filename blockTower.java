fb6f58ad18fe30e474ee3152aaec0062209fb823
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-08-07 22:15:25 +0930

    BlockTower tested on arena

--- blockTower.java
+++ blockTower.java
@@ -2,17 +2,24 @@ package TopCoder;
 
 public class BlockTower {
 	public static void main(String[]args){
-		int []blockHeights = {24, 20, 3, 9, 20, 12, 27, 16, 30, 29, 43, 8, 27, 38, 42, 1, 25, 34, 8, 23, 27, 22, 47, 39, 41, 41, 44, 12, 24, 36, 19, 37, 46, 9, 27, 22, 34, 12, 46, 41, 36, 42, 11, 45, 20, 2, 27, 46, 50, 19};
+		int []blockHeights = 
+			{48,1,50,1,50,1,48}
+//			{24, 20, 3, 9, 20, 12, 27, 16, 30, 29, 43, 8, 27, 38, 42, 1, 25, 34, 8, 23, 27, 22, 47, 39, 41, 41, 44, 12, 24, 36, 19, 37, 46, 9, 27, 22, 34, 12, 46, 41, 36, 42, 11, 45, 20, 2, 27, 46, 50, 19}
+		;
 		int res = getTallest(blockHeights);
 		System.out.println(res);
 	}
 	public static int getTallest(int[] blockHeights){
 		int res = 0;
-		for (int i = 0; i < blockHeights.length; i++) {
+		for (int i = 0; i <= blockHeights.length; i++) {
 			int temp = 0;
 			int j = 0;
-			for (; j < i; j++) if (blockHeights[j]%2==0) temp+=blockHeights[j];
-			for (; j < blockHeights.length; j++) if (blockHeights[j] %2!=0) temp+=blockHeights[j];
+			for (; j < i; j++) 
+				if (blockHeights[j]%2==0) 
+					temp+=blockHeights[j];
+			for (; j < blockHeights.length; j++) 
+				if (blockHeights[j] %2==1) 
+					temp+=blockHeights[j];
 			res = Math.max(res, temp);
 		}
 		return res;
