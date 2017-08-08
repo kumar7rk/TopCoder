0bfce9c87a275ce1b0342594cdff484ae5dd693b
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-08-08 17:23:56 +0930

    CoinFlipsDiv2 tested on arena

--- CoinFlipsDiv2.java
+++ CoinFlipsDiv2.java
@@ -1,19 +1,22 @@
 package TopCoder;
+
 public class CoinFlipsDiv2 {
 	public static void main(String[] args) {
-		String state = "THTTTTTTTTTTTTTHTTHTTTTTHHHTTTTHTTTTTTTTTT";
+		String state = "H";
 		int res = countCoins(state);
 		System.out.println(res);
 	}
 	public static int countCoins(String state){
 		int count = 0;
-		if (state.charAt(0)!=state.charAt(1) || state.charAt(state.length()-2)!=state.charAt(state.length()-1)) count++;
-		for (int i = 1; i < state.length()-1; i++) {
-			char c_1 = state.charAt(i-1);
-			char c1 = state.charAt(i);
-			char c2 = state.charAt(i+1);
-			if (c1!=c2 || c1!=c_1) count++;
+		if (state.length()==1) return 0;
+		for (int i = 0; i < state.length(); i++) {
+			int i1 = i-1;
+			int i2 = i+1;
+			if (i==0) i1 = i2; 
+			if (i==state.length()-1) i2=i1;
+			if (state.charAt(i)!=state.charAt(i1) | state.charAt(i)!=state.charAt(i2)) count++;
 		}
 		return count;
+		
 	}
 }
