91dcfa31f168a6d7aeffac0a1ba666bb3ee10f77
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-08-14 05:33:00 +0930

    ClockWalk tested on arena

--- ClockWalk.java
+++ ClockWalk.java
@@ -3,16 +3,27 @@ package TopCoder;
 public class ClockWalk {
 
 	public static void main(String[] args) {
-		String flips = "hthhthtththhtttthttthhhthtttthh";
+		String flips = "hththththththththththth";
 		int res = finalPosition(flips);
-		System.out.println(res);
+		System.out.println("RES: "+res);
 	}
 	public static int finalPosition(String flips) {
 		int hr = 12;
-		for (int i = 1; i < flips.length()+1; i++) {
-			if (flips.charAt(i-1)=='h') hr+=i;
-			if (flips.charAt(i-1)=='t') hr-=i;
-			if (hr>12|| hr<1) hr %=12;
+		for (int i = 1; i <= flips.length(); i++) {
+			if (flips.charAt(i-1)=='h'){ 
+				hr+=i;
+				if (hr>12){
+					hr %=12;
+				}
+			}
+			if (flips.charAt(i-1)=='t'){
+				hr-=i;
+				if (hr<1){
+					hr = Math.abs(hr)%12;
+					hr=12-hr;
+				}
+			}
+			System.out.println(hr);
 		}
 		return hr;
 	}
