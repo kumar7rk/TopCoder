9b21863e3bd54dc81509ec5e3abf7eaa37bfa284
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-08-21 22:36:56 +0930

    Chivalry added

---
+++ Chivalry.java
@@ -0,0 +1,48 @@
+package TopCoder;
+
+import java.util.ArrayList;
+
+public class Chivalry {
+
+	public static void main(String[] args) {
+		String first = "MM";
+		String second = "MW";
+		String res = getOrder(first,second);
+		System.out.println(res);
+	}
+
+	public static String getOrder(String first, String second) {
+		ArrayList<Character> arrayList1 = new ArrayList<>();
+		ArrayList<Character> arrayList2 = new ArrayList<>();
+//		ArrayList<Character> res = new ArrayList<>();
+		
+		String res = "";
+		
+		int n = first.length();
+		int m = second.length();
+		
+		int count1 = 0;
+		int count2 = 0;
+		
+		while (n>count1&&m>count2) {
+			char c1 = first.charAt(count1);
+			char c2 = second.charAt(count2);
+			if (c1=='M'&&c2=='W') {
+				res+=c2;
+				count2++;
+			}
+			else {
+				res+=c1;
+				count1++;
+			}
+		}
+		if (count1<n) {
+			res+=first.substring(count1);
+		}
+		else {
+			res+=second.substring(count2);
+		}
+		return res;
+	}
+
+}
