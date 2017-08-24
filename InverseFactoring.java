b7e1017fcfa3563cb25609cee1cd6806bbd45b80
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-08-24 15:53:38 +0930

    InverseFactoring not tested on arena- SRM day

---
+++ InverseFactoring.java
@@ -0,0 +1,17 @@
+package TopCoder;
+
+import java.util.Arrays;
+
+public class InverseFactoring {
+
+	public static void main(String[] args) {
+		int[] factors = {14,26456,2,28,13228,3307,7,23149,8,6614,46298,56,4,92596};
+		int res = getTheNumber(factors);
+		System.out.println(res);
+	}
+	public static int getTheNumber(int[] factors) {
+		Arrays.sort(factors);
+		return factors[0]*factors[factors.length-1];
+	}
+
+}
