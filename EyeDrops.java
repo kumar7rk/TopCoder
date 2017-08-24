0eefe15d547db2c93f8b1bb4fe3b3b46a8d685e7
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-08-24 16:23:31 +0930

    EyeDrops w/out testing on arena- SRM day

---
+++ EyeDrops.java
@@ -0,0 +1,16 @@
+package TopCoder;
+
+public class EyeDrops {
+
+	public static void main(String[] args) {
+		int sleepTime = 23;
+		int k = 1;
+		double res = closest(sleepTime,k);
+		System.out.println(res);
+	}
+	public static double closest(int sleepTime, int k) {
+		if(sleepTime>24/k) return (24-sleepTime)*60/(k-1);
+		return 1440/k;
+	}
+
+}
