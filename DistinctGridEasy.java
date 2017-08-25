5e77dce1ae35c5362a71b3f62b2ee795464a655b
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-08-25 17:12:57 +0930

    DistinctGridEasy added

--- DistinctGridEasy.java
+++ DistinctGridEasy.java
@@ -7,18 +7,14 @@ public class DistinctGridEasy {
 	public static void main(String[] args) {
 		int n = 5;
 		int k = 3;
-		int[] grid = 
-				{ 2,2,0,0,1, 0,1,2,2,0, 0,2,1,0,0, 1,0,0,0,2, 0,0,2,1,0 }
-		;
+		int[] grid = { 2,2,0,0,1, 0,1,2,2,0, 0,2,1,0,0, 1,0,0,0,2, 0,0,2,1,0 };
 		String res = checkGrid(n,k,grid);
 		System.out.println(res);
-
 	}
-
 	public static String checkGrid(int n, int k, int[] grid) {
 		int start = 0;
 		int start1 = 0;
-		while(start<grid.length){
+		while(start<n){
 			HashSet<Integer> row = new HashSet<>();
 			for (int j = start; j < start+n; j++) row.add(grid[j]);
 			if (row.size()!=k) return "Bad";
@@ -26,9 +22,9 @@ public class DistinctGridEasy {
 		}
 		while(start1<n){
 			HashSet<Integer> column = new HashSet<>();
-			for (int i = start1; i < grid.length; i+=start1) column.add(grid[i]);
-			start1++;
+			for (int i = start1; i < grid.length; i+=n) column.add(grid[i]);
 			if (column.size()!=k) return "Bad";
+			start1++;
 		}
 		return "Good";
 	}
