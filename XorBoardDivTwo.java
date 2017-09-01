794312ac460a85f166f9111d9055b898ea8c5a1e
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-09-01 17:34:41 +0930

    XorBoardDivTwo added

--- XorBoardDivTwo.java
+++ XorBoardDivTwo.java
@@ -1,3 +1,4 @@
+package TopCoder;
 public class XorBoardDivTwo{
 	public static void main(String[] args){
 		String[] board = 
@@ -7,11 +8,40 @@ public class XorBoardDivTwo{
 		System.out.println(res);
 	}
 	public static int theMax(String[]board){
-		String[][]board2 = new String[board.length][board[0].length()];
+		int max = Integer.MIN_VALUE;
+		int [][]b = new int[board.length][board[0].length()]; 
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[0].length(); j++) {
+				int count =0;
+				for (int k = 0; k < board.length; k++) {
+					for (int l = 0; l < board[0].length(); l++) {
+						if (board[k].charAt(l)=='1') b[k][l] = 1;
+						else b[k][l]= 0;
+					}
+				}
+				
+				for (int k = 0; k < board[0].length(); k++) {
+					if (b[i][k]==0) b[i][k] = 1;
+					else b[i][k] = 0;
+				}
+				for (int k = 0; k < board.length; k++) {
+					if (b[k][j]==0) b[k][j] = 1;
+					else b[k][j] = 0;
+				}
+				for (int k = 0; k < board.length; k++) {
+					for (int l = 0; l < board[0].length(); l++) {
+						if(b[k][l]==1) count++;
+					}
+				}
+				if(count>max) max = count;
+			}
+		}
+		return max;
+	}
+}
+/*String[][]board2 = new String[board.length][board[0].length()];
 		int row = 0;
 		int col = 0;
-		int ones_in_row = 0;
-		int ones_in_col = 0;
 		int max_zeros_row = -1;
 		int max_zeros_col = -1;
 		
@@ -20,7 +50,10 @@ public class XorBoardDivTwo{
 				board2[i][j] = board[i].charAt(j)+"";
 			}
 		}
+		
+		// row- finding row with minimum number of 0
 		for(int i = 0; i< board.length;i++){
+			
 			int zeros_in_row = 0;
 			for(int j = 0; j<board[0].length();j++){
 				char c = board[i].charAt(j);
@@ -31,12 +64,14 @@ public class XorBoardDivTwo{
 				row = i;
 			} 
 		}
+		// changing a row with least zero
 		for(int i = 0; i< board[row].length();i++){
 			if(board2[row][i].equals("0"))
 				board2[row][i] = "1";
 			else if(board2[row][i].equals("1"))
 				board2[row][i] = "0";
 		}
+		
 		System.out.println("Row: "+ row);
 		for(int i = 0; i< board.length;i++){
 			for(int j = 0; j<board[0].length();j++){
@@ -44,6 +79,7 @@ public class XorBoardDivTwo{
 			}
 			System.out.println();
 		}
+//		column- finding column with minimum number of 0
 		for(int i = 0; i< board[0].length() ;i++){
 			int zeros_in_col = 0;
 			for(int j = 0; j<board.length;j++){
@@ -55,6 +91,7 @@ public class XorBoardDivTwo{
 				col = i;
 			} 
 		}
+		// changing a column with least zero
 		for(int i = 0; i< board.length;i++){
 			if(board2[i][col].equals("0"))
 				board2[i][col] = "1";
@@ -75,5 +112,4 @@ public class XorBoardDivTwo{
 			}
 		}		
 		return ones;
-	}
-}
+*/
