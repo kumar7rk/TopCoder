d4ae3dcd6aff14a9f7789e9d210b192b099507bf
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-09-03 20:58:30 +0930

    Structure only:

---
+++ GoodCompanyDivTwo.java
@@ -0,0 +1,10 @@
+package TopCoder;
+public class GoodCompanyDivTwo{
+	public static void main(String[] args){
+		
+	}
+	public static int countGood(int[] superior, int[] workTyp2){
+		return 0;
+	}
+	
+}
