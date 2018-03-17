4618d8a3c233a734c0d14c591fff39317f7f4e5a
Rohit Kumar <rohitkumarrk1992@gmail.com>
2018-03-17 20:23:27 +1030

    IntervalIntersections added not tested

---
+++ IntervalIntersections.java
@@ -0,0 +1,14 @@
+public class IntervalIntersections{
+
+	public static int minLength(int x1, int y1, int x2, int y2){
+		if(x1<=y2&&x1>=x2) return 0;
+		if(y1<=x2&&y1>=y2) return 0;
+		if(x2<=y1&&x2>=x1) return 0;
+		if(y2<=y1&&y1>=x1) return 0;
+		
+		if(y1<x2) return x2-y1;
+		else return x1-y2;
+		
+	}
+}
+
