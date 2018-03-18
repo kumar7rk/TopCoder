187bc758d50501a5eaa5d9e1b50560d4dd844498
Rohit Kumar <rohitkumarrk1992@gmail.com>
2018-03-18 22:40:02 +1030

    IntervalIntersections added
    tested on arena

--- IntervalIntersections.java
+++ IntervalIntersections.java
@@ -4,7 +4,7 @@ public class IntervalIntersections{
 		if(x1<=y2&&x1>=x2) return 0;
 		if(y1<=x2&&y1>=y2) return 0;
 		if(x2<=y1&&x2>=x1) return 0;
-		if(y2<=y1&&y1>=x1) return 0;
+		if(y2<=y1&&y2>=x1) return 0;
 		
 		if(y1<x2) return x2-y1;
 		else return x1-y2;
